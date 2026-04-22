78823a65949a9ac7f3591d3249e29e106187b359
Gabe <Carrollgbusiness@gmail.com>
2026-04-22 15:01:20 -0500

    Added Product Objects

--- src/main/java/com/pluralsight/SearchApp.java
+++ src/main/java/com/pluralsight/SearchApp.java
@@ -5,7 +5,12 @@ import java.util.ArrayList;
 public class SearchApp {
     public static void main(String[] args) {
         //Create ArrayList of Product Objects
-        ArrayList<String> products = new ArrayList<String>();
+        Product product1 = new Product(01, "The Beatles: Abbey Road (1969)", 29.99);
+        Product product2 = new Product(02, "Crosley CR8005D Cruiser Deluxe Turntable", 89.99);
+        Product product3 = new Product(03, "Sony Walkman TPS-L2 Replica Cassette Player", 49.99);
+        Product product4 = new Product(04, "Modway Render Vinyl Record Display Stand", 234.99);
+        Product product5 = new Product(05, "QFX Shoebox-Style Cassette Tape Player/Recorder", 47.99);
+        ArrayList<String> productList = new ArrayList<String>();
 
         //call getInventory to fill the list
 
