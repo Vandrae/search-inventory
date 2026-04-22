5c04f23b07944016b1b0b93f84824d49bc010319
Gabe <Carrollgbusiness@gmail.com>
2026-04-22 15:48:24 -0500

    Display Product list properly

--- src/main/java/com/pluralsight/SearchApp.java
+++ src/main/java/com/pluralsight/SearchApp.java
@@ -5,11 +5,13 @@ import java.util.ArrayList;
 public class SearchApp {
     public static void main(String[] args) {
         //Create ArrayList of Product Objects
-        Product product1 = new Product(01, "The Beatles: Abbey Road (1969)", 29.99);
-        Product product2 = new Product(02, "Crosley CR8005D Cruiser Deluxe Turntable", 89.99);
-        Product product3 = new Product(03, "Sony Walkman TPS-L2 Replica Cassette Player", 49.99);
-        Product product4 = new Product(04, "Modway Render Vinyl Record Display Stand", 234.99);
-        Product product5 = new Product(05, "QFX Shoebox-Style Cassette Tape Player/Recorder", 47.99);
+        Product product1 = new Product(1, "The Beatles: Abbey Road (1969)", 29.99);
+        Product product2 = new Product(2, "Crosley CR8005D Cruiser Deluxe Turntable", 89.99);
+        Product product3 = new Product(3, "Sony Walkman TPS-L2 Replica Cassette Player", 49.99);
+        Product product4 = new Product(4, "Modway Render Vinyl Record Display Stand", 234.99);
+        Product product5 = new Product(5, "QFX Shoebox-Style Cassette Tape Player/Recorder", 47.99);
+
+        //call getInventory to fill the list
         ArrayList<Product> productList = new ArrayList<Product>();
         productList.add(product1);
         productList.add(product2);
@@ -18,9 +20,11 @@ public class SearchApp {
         productList.add(product5);
         System.out.println(productList);
 
-        //call getInventory to fill the list
-
         //display all products
+        for (int i = 0; i < productList.size();i++){
+            System.out.println( productList.get(i).getId() + " " + productList.get(i).getName() + " " + productList.get(i).getPrice());
+        }
+
     }
 
     //getInvintrory Method
