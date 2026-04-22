ccb9589f6484a6c52d821fa3226f80c093d82ae8
Gabe <Carrollgbusiness@gmail.com>
2026-04-22 14:45:19 -0500

    First Commit

--- src/main/java/com/pluralsight/Product.java
+++ src/main/java/com/pluralsight/Product.java
@@ -1,4 +1,42 @@
 package com.pluralsight;
 
 public class Product {
+    //descriptors
+    int id;
+    String name;
+    double price;
+
+    //constructor
+
+    public Product(int id, String name, double price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    //getter&setters
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
 }
--- src/main/java/com/pluralsight/SearchApp.java
+++ src/main/java/com/pluralsight/SearchApp.java
@@ -1,4 +1,20 @@
 package com.pluralsight;
 
+import java.util.ArrayList;
+
 public class SearchApp {
+    public static void main(String[] args) {
+        //Create ArrayList of Product Objects
+        ArrayList<String> products = new ArrayList<String>();
+
+        //call getInventory to fill the list
+
+        //display all products
+    }
+
+    //getInvintrory Method
+        //create 5+ product objects
+        //add each one to the array list
+        //return the array list
+
 }
